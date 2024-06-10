ab2d06bda3fc3b593e71e3f158227589428fa86a
dungnn <tienphongcutin@gmail.com>
2024-06-11 00:52:34 +0700

    up

---
+++ src/app/about/our-team/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// components
+import OurTeam from '@/components/pages/our-team/our-team-threeland';
+import OurTeamLogo from '@/components/pages/our-team/our-team-logo';
+
+const Page = () => {
+  return (
+    <main>
+        <OurTeam />
+
+        <OurTeamLogo />
+    </main>
+  );
+};
+
+export default Page;
